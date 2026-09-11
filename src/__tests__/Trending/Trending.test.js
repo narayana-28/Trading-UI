36e8cf4c8d1245d0280eac53440cc0fb8a755a51
narayana-28 <kadalakshminarayana@gmail.com>
2026-09-11 15:45:11 +0530

    Refactor Trending test to mock axios and improve syntax

--- src/__tests__/Trending/Trending.test.js
+++ src/__tests__/Trending/Trending.test.js
@@ -1,17 +1,26 @@
-import React,{Component} from 'react';
+import React from 'react';
 import { shallow } from 'enzyme';
+import axios from 'axios';
 import Trending from '../../Components/Trending/Trending';
 
+jest.mock('axios');
 
-describe('when the home component is called',()=>{
+describe('when the home component is called', () => {
     let wrapper;
-    beforeEach(()=>{
-        wrapper=shallow(<Trending/>);
+
+    beforeEach(() => {
+        axios.get.mockResolvedValue({
+            status: 200,
+            data: {
+                status: 'SUCCESS',
+                data: []
+            }
+        });
+
+        wrapper = shallow(<Trending />);
     });
-    it('should render the render method',()=>{
+
+    it('should render the render method', () => {
         expect(wrapper).toHaveLength(1);
     });
-    
-   
-})
-
+});
