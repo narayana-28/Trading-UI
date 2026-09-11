c5e65a329ab907fa86fd8cde4770be640b85cfa9
narayana-28 <kadalakshminarayana@gmail.com>
2026-09-11 16:03:19 +0530

    Update ListStocks.test.js

--- src/__tests__/ListStocks/ListStocks.test.js
+++ src/__tests__/ListStocks/ListStocks.test.js
@@ -1,15 +1,26 @@
-import React,{Component} from 'react';
-import { shallow } from 'enzyme';import Purchase from '../../Components/Purchase/Purchase';
+import React from 'react';
+import { shallow } from 'enzyme';
+import axios from 'axios';
 import ListStocks from '../../Components/ListStocks/ListStocks';
 
-describe('when the home component is called',()=>{
+jest.mock('axios');
+
+describe('when the home component is called', () => {
     let wrapper;
-    beforeEach(()=>{
-        wrapper=shallow(<ListStocks/>);
+
+    beforeEach(() => {
+        axios.get.mockResolvedValue({
+            status: 200,
+            data: {
+                status: 'SUCCESS',
+                data: []
+            }
+        });
+
+        wrapper = shallow(<ListStocks />);
     });
-    it('should render the render method',()=>{
+
+    it('should render the render method', () => {
         expect(wrapper).toHaveLength(1);
     });
-   
-})
-
+});
